bcff3fde31a96856caf2a5379d3ee7d09f1dd351
iman safari <imansafari33@gmail.com>
2021-05-13 12:20:45 +0430

    add user model

---
+++ jwt-authentication/jwt-authentication/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jwt_authentication.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string UserName { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Password { get; set; }
+        public string  Email { get; set; }
+        public string Mobile { get; set; }
+        public DateTime CreateDate { get; set; }
+        
+    }
+}
